290d5693ccd21277f8688041ebadd4e825bba30c
Sako M <sakom@Sako-M2.local>
2024-04-20 20:49:17 -0700

    update

--- agent/prompt_runner.py
+++ agent/prompt_runner.py
@@ -6,9 +6,9 @@ from llama_index.core.llms import ChatMessage
 
 def get_query_from_user(prompt): 
   llm = Solar(api_key=os.environ["SOLAR_API_KEY"], is_chat_model=True)
-
+  rag=open('../data/data.txt', 'r').read()
   response = llm.chat(messages=[
-    ChatMessage(role="system", content="You are a helpful assistant."),
+    ChatMessage(role="system", content="Hello! I am here to help you with your Jira queries. Mention that queried the Mongodb for data. just give answer, make it short"+rag),
     ChatMessage(role="user", content=prompt)
   ])
   return response
