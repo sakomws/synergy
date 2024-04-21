0d2a798d8e26abaca1dec70a6056e1c925867b83
Sako M <sakom@Sako-M2.local>
2024-04-20 20:23:46 -0700

    Add upstage

--- agent/prompt_runner.py
+++ agent/prompt_runner.py
@@ -1,48 +1,14 @@
 import json
 import requests
 import os
+from llama_index.llms.solar import Solar
+from llama_index.core.llms import ChatMessage
 
-def get_query_from_user(prompt):
-  stream = False
-  url = "https://proxy.tune.app/chat/completions"
-  headers = {
-      "Authorization": os.environ.get("TUNE_API_KEY"),
-      "Content-Type": "application/json",
-  }
-  rag=open('../data/data.txt', 'r').read()
-  data = {
-    "temperature": 0.8,
-      "messages":  [
-    {
-      "role": "system",
-      "content": "Welcome to Synergy AI! "
-    },
-    {
-      "role": "user",
-      "content": rag
-    },
-    {
-      "role": "user",
-      "content": prompt  
-    },
-    {
-      "role": "assistant"
-      
-    },
-  
-  ],
-      "model": "rohan/mixtral-8x7b-inst-v0-1-32k",
-      "stream": stream,
-      "penalty":  0,
-      "max_tokens": 900
-  }
-  response = requests.post(url, headers=headers, json=data)
-  
-  if response.status_code == 200:
-    json_response = response.json()
-    # Access the assistant's response content directly
-    answer = json_response['choices'][0]['message']['content']
-    return answer
-  else:
-    print(f"Error: {response.status_code}")
-    return "Cannot generate response at the moment. Please try again later."
+def get_query_from_user(prompt): 
+  llm = Solar(api_key=os.environ["SOLAR_API_KEY"], is_chat_model=True)
+
+  response = llm.chat(messages=[
+    ChatMessage(role="system", content="You are a helpful assistant."),
+    ChatMessage(role="user", content=prompt)
+  ])
+  return response
